5325827d80fb01676fda531a81a46f5dd516a221
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-05-26 18:10:27 +0300

    Тесты на селекторы и оставшиеся исправления

--- src/ChatActions.test.ts
+++ src/ChatActions.test.ts
@@ -1,135 +1,163 @@
 import { Store, configureStore } from "@reduxjs/toolkit";
 import rootChatReducer from "./ChatReducer";
-import { ChatMessage, ChatState, ChatStatus, ChatUser, initialState } from "./ChatState";
-import { ChatAction, fetchMessageFailure, fetchMessageRequest, fetchMessageSuccess, fetchMessagesFailure, fetchMessagesRequest, fetchMessagesSuccess, fetchUsersFailure, fetchUsersRequest, fetchUsersSuccess, searchMessages, sendMessageFailure, sendMessageRequest, sendMessageSuccess } from "./ChatActions";
-import ChatError from "./ChatError";
-import { fetchMessages } from "./ExternalApiMock";
+import {
+  ChatError,
+  ChatMessage,
+  ChatState,
+  ChatStatus,
+  ChatUser,
+  initialState,
+} from "./ChatState";
+import {
+  ChatAction,
+  fetchMessageFailure,
+  fetchMessageRequest,
+  fetchMessageSuccess,
+  fetchMessagesFailure,
+  fetchMessagesRequest,
+  fetchMessagesSuccess,
+  fetchUsersFailure,
+  fetchUsersRequest,
+  fetchUsersSuccess,
+  searchMessages,
+  sendMessageFailure,
+  sendMessageRequest,
+  sendMessageSuccess,
+} from "./ChatActions";
 
 describe("Test for Actions", () => {
-    let store: Store<ChatState, ChatAction>;
-    const testError = new ChatError("Test Error");
+  let store: Store<ChatState, ChatAction>;
+  const testError: ChatError = {
+    date: new Date().toISOString(),
+    message: "This is test error record",
+  };
 
-    function generateChatUser(index: number): ChatUser {
-      return {
-        login: `login-${index}`,
-        name: `USER-${index}`,
-        email: `user${index}@email.com`,
-        role: Math.floor(Math.random() * 3),
-      };
-    }
-    
-    function generateChatMessage(index: number): ChatMessage {
-      const user = generateChatUser(index);
-      return {
-        author: user,
-        text: `It's generated message from ${user.name}`,
-        date: new Date().toISOString(),
-      };
-    }
-    
-    function generateChatMessages(size: number): ChatMessage[] {
-      return [...Array(size).keys()].map((index) => generateChatMessage(index));
-    }
-    
-    function generateChatUsers(size: number): ChatUser[] {
-      return [...Array(size).keys()].map((index) => generateChatUser(index));
-    }
+  function generateChatUser(index: number): ChatUser {
+    return {
+      login: `login-${index}`,
+      name: `USER-${index}`,
+      email: `user${index}@email.com`,
+      role: Math.floor(Math.random() * 3),
+    };
+  }
 
-    beforeEach(() => {
-        store = configureStore({reducer: rootChatReducer, preloadedState: initialState});
-    })
+  function generateChatMessage(index: number): ChatMessage {
+    const user = generateChatUser(index);
+    return {
+      author: user,
+      text: `It's generated message from ${user.name}`,
+      date: new Date().toISOString(),
+    };
+  }
 
-    it("Checks for reducer initial state", () => {
-      const testState: ChatState = {
-        messages: [],
-        errors: [],
-        users: [],
-        status: ChatStatus.ERROR
-      };
+  function generateChatMessages(size: number): ChatMessage[] {
+    return [...Array(size).keys()].map((index) => generateChatMessage(index));
+  }
 
-      store = configureStore({reducer: rootChatReducer});
-      expect(store.getState()).toEqual(initialState);
+  function generateChatUsers(size: number): ChatUser[] {
+    return [...Array(size).keys()].map((index) => generateChatUser(index));
+  }
 
-      store = configureStore({reducer: rootChatReducer, preloadedState: testState});
-      expect(store.getState()).toEqual(testState);
-    })
-    
-    it.each([
-      [fetchMessageRequest()],
-      [fetchMessagesRequest()],
-      [fetchUsersRequest()],
-      [sendMessageRequest()],
-    ])("Check request action for %p", (action) => {
-      store.dispatch(action);
-      expect(store.getState().status).toBe(ChatStatus.LOADING);
+  beforeEach(() => {
+    store = configureStore({
+      reducer: rootChatReducer,
+      preloadedState: initialState,
     });
+  });
 
-    it.each([
-      [fetchMessageFailure(testError)],
-      [fetchMessagesFailure(testError)],
-      [fetchUsersFailure(testError)],
-      [sendMessageFailure(testError)],
-    ])("Check failure action for %p", (action) => {
-      store.dispatch(action);
-      expect(store.getState().status).toBe(ChatStatus.ERROR);
-      expect(store.getState().errors[0]).toBe(testError);
+  it("Checks for reducer initial state", () => {
+    const testState: ChatState = {
+      messages: [],
+      errors: [],
+      users: [],
+      status: ChatStatus.ERROR,
+    };
+
+    store = configureStore({ reducer: rootChatReducer });
+    expect(store.getState()).toEqual(initialState);
+
+    store = configureStore({
+      reducer: rootChatReducer,
+      preloadedState: testState,
     });
+    expect(store.getState()).toEqual(testState);
+  });
 
-    it("Check fetch message action", () => {
-      const message1 = generateChatMessage(0);
-      const message2 = generateChatMessage(1);
+  it.each([
+    [fetchMessageRequest()],
+    [fetchMessagesRequest()],
+    [fetchUsersRequest()],
+    [sendMessageRequest()],
+  ])("Check request action for %p", (action) => {
+    store.dispatch(action);
+    expect(store.getState().status).toBe(ChatStatus.LOADING);
+  });
 
-      store.dispatch(fetchMessageSuccess(message1));
-      expect(store.getState().status).toBe(ChatStatus.READY);
-      expect(store.getState().messages).toEqual([message1]);
-      
-      store.dispatch(fetchMessageSuccess(message2));
-      expect(store.getState().messages).toEqual([message1, message2]);
-    })
+  it.each([
+    [fetchMessageFailure(testError)],
+    [fetchMessagesFailure(testError)],
+    [fetchUsersFailure(testError)],
+    [sendMessageFailure(testError)],
+  ])("Check failure action for %p", (action) => {
+    store.dispatch(action);
+    expect(store.getState().status).toBe(ChatStatus.ERROR);
+    expect(store.getState().errors[0]).toBe(testError);
+  });
 
-    it("Check fetch messages action", () => {
-      const message1 = generateChatMessages(5);
-      const message2 = generateChatMessages(5);
+  it("Check fetch message action", () => {
+    const message1 = generateChatMessage(0);
+    const message2 = generateChatMessage(1);
 
-      store.dispatch(fetchMessagesSuccess(message1));
-      expect(store.getState().status).toBe(ChatStatus.READY);
-      expect(store.getState().messages).toEqual(message1);
-      
-      store.dispatch(fetchMessagesSuccess(message2));
-      expect(store.getState().messages).toEqual([...message1, ...message2]);
-    })
+    store.dispatch(fetchMessageSuccess(message1));
+    expect(store.getState().status).toBe(ChatStatus.READY);
+    expect(store.getState().messages).toEqual([message1]);
 
-    it("Check fetch users action", () => {
-      const users1 = generateChatUsers(5);
-      const users2 = generateChatUsers(5);
+    store.dispatch(fetchMessageSuccess(message2));
+    expect(store.getState().messages).toEqual([message1, message2]);
+  });
 
-      store.dispatch(fetchUsersSuccess(users1));
-      expect(store.getState().status).toBe(ChatStatus.READY);
-      expect(store.getState().users).toEqual(users1);
+  it("Check fetch messages action", () => {
+    const message1 = generateChatMessages(5);
+    const message2 = generateChatMessages(5);
 
-      store.dispatch(fetchUsersSuccess(users2));
-      expect(store.getState().users).toEqual(users2);
-    })
+    store.dispatch(fetchMessagesSuccess(message1));
+    expect(store.getState().status).toBe(ChatStatus.READY);
+    expect(store.getState().messages).toEqual(message1);
 
-    it("Check send message action", () => {
-      const message1 = generateChatMessage(0);
-      const message2 = generateChatMessage(1);
+    store.dispatch(fetchMessagesSuccess(message2));
+    expect(store.getState().messages).toEqual([...message1, ...message2]);
+  });
 
-      store.dispatch(sendMessageSuccess(message1));
-      expect(store.getState().status).toBe(ChatStatus.READY);
-      expect(store.getState().messages).toEqual([message1]);
+  it("Check fetch users action", () => {
+    const users1 = generateChatUsers(5);
+    const users2 = generateChatUsers(5);
 
-      store.dispatch(sendMessageSuccess(message2));
-      expect(store.getState().messages).toEqual([message1, message2]);
-    })
+    store.dispatch(fetchUsersSuccess(users1));
+    expect(store.getState().status).toBe(ChatStatus.READY);
+    expect(store.getState().users).toEqual(users1);
 
-    it("Check search messages action", () => {
-      const filter = (message: ChatMessage) => message.author.name === "USER-0";
-      const messages = generateChatMessages(5);
+    store.dispatch(fetchUsersSuccess(users2));
+    expect(store.getState().users).toEqual(users2);
+  });
 
-      store.dispatch(fetchMessagesSuccess(messages));
-      store.dispatch(searchMessages(filter));
-      expect(store.getState().status).toBe(ChatStatus.READY);
-      expect(store.getState().messages).toEqual([messages[0]]);
-    })
-})
+  it("Check send message action", () => {
+    const message1 = generateChatMessage(0);
+    const message2 = generateChatMessage(1);
+
+    store.dispatch(sendMessageSuccess(message1));
+    expect(store.getState().status).toBe(ChatStatus.READY);
+    expect(store.getState().messages).toEqual([message1]);
+
+    store.dispatch(sendMessageSuccess(message2));
+    expect(store.getState().messages).toEqual([message1, message2]);
+  });
+
+  it("Check search messages action", () => {
+    const messages = generateChatMessages(5);
+
+    store.dispatch(fetchMessagesSuccess(messages));
+    store.dispatch(searchMessages("USER-0"));
+    expect(store.getState().status).toBe(ChatStatus.READY);
+    expect(store.getState().messages).toEqual([messages[0]]);
+  });
+});
